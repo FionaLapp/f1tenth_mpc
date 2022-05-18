1118cf658a6de0af9de0363bf055aba2b9607d35
FionaLapp <fiona.lapp@web.de>
2022-05-18 16:52:03 +0100

    Reading parameters from yaml file

--- src/mpc.py
+++ src/mpc.py
@@ -26,10 +26,7 @@ import rospy
 from sensor_msgs.msg import Image, LaserScan
 from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
 
-#PARAMS
-ANGLE_RANGE = 270 # Hokuyo 10LX has 270 degrees scan
-VELOCITY = 2.00 # meters per second
-CAR_LENGTH = 0.50 # Traxxas Rally is 20 inches or 0.5 meters
+
 
 
 class MPC:
@@ -39,13 +36,41 @@ class MPC:
         #Topics & Subs, Pubs
         lidarscan_topic = '/scan'
         drive_topic = '/nav'
-
+        self.params=self.get_params()
         self.lidar_sub = rospy.Subscriber(lidarscan_topic, LaserScan, self.lidar_callback)#TODO: Subscribe to LIDAR
         
         self.drive_pub = rospy.Publisher(drive_topic, AckermannDriveStamped, queue_size=1)#TODO: Publish to drive
         #self.get_mpc_step()
         self.initialize_mpc()
-        
+
+    def get_params(self):
+        #I quite possibly came up with the worst way to do this
+        rospy.loginfo("Getting params")
+        params={}
+        namespace='mpc/'
+        params['wheelbase']= rospy.get_param(namespace+'wheelbase', 0.3302) # meters
+        params['width']= rospy.get_param(namespace+'width', 0.2032) # meters (width of racecar)
+        params['buffer_length']= rospy.get_param(namespace+'buffer_length', 5) # steering delay
+        params['max_speed']=rospy.get_param(namespace+'max_speed', 7) #  meters/second
+        params['max_steering_angle']= rospy.get_param(namespace+'max_steering_angle',  0.4189 )# radians
+        params['max_accel']= rospy.get_param(namespace+'max_accel', 7.51) # meters/second^2
+        params['max_decel']= rospy.get_param(namespace+'max_decel',  8.26) # meters/second^2
+        params['max_steering_vel']= rospy.get_param(namespace+'max_steering_vel',  3.2) # radians/second
+        params['friction_coeff']= rospy.get_param(namespace+'friction_coeff', 0.523) # - (complete estimate)
+        params['height_cg']= rospy.get_param(namespace+'height_cg', 0.074) # m (roughly measured to be 3.25 in)
+        params['l_cg2rear']= rospy.get_param(namespace+'l_cg2rear', 0.17145) # m (decently measured to be 6.75 in)
+        params['l_cg2front']= rospy.get_param(namespace+'l_cg2front', 0.15875) # m (decently measured to be 6.25 in)
+        params['C_S_front']= rospy.get_param(namespace+'C_S_front', 4.718) #.79 # 1/rad ? (estimated weight/4)
+        params['C_S_rear']= rospy.get_param(namespace+'C_S_rear', 5.4562) #.79 # 1/rad ? (estimated weight/4)
+        params['mass']= rospy.get_param(namespace+'mass', 3.47) # kg (measured on car 'lidart')
+        params['moment_inertia']= rospy.get_param(namespace+'moment_inertia', .04712) # kg m^2 (estimated as a rectangle with width and height of car and evenly distributed mass, then shifted to account for center of mass location)
+        params['update_pose_rate']= rospy.get_param(namespace+'update_pose_rate', 0.001) # The rate at which the pose and the lidar publish
+        # Lidar simulation parameters
+        params['scan_beams']= rospy.get_param(namespace+'scan_beams', 1080)
+        params['scan_field_of_view']= rospy.get_param(namespace+'scan_field_of_view', 6.2831853) #4.71 # radians
+        # The distance from the center of the rear axis (base_link) to the lidar
+        params['scan_distance_to_base_link']= rospy.get_param(namespace+'scan_distance_to_base_link', 0.275) # meters
+        return params    
 
     def getRange(self, data, angle):
         # data: single message from topic /scan
@@ -108,8 +133,8 @@ class MPC:
         Next we define parameters. Known values can and should be hardcoded but with robust MPC in mind, we define uncertain parameters explictly. We assume that the inertia is such an uncertain parameter and hardcode the spring constant and friction coefficient.
         '''
 
-        l_r=1
-        l_f=1
+        l_r=self.params['l_cg2rear']
+        l_f=self.params['l_cg2front']
         l=l_r+l_f
         beta=self.model.set_expression('beta', np.arctan((l_r/l)*np.tan(delta)))
 
@@ -161,12 +186,14 @@ class MPC:
         self.mpc.set_objective(mterm=mterm, lterm=lterm)
 
         # Constraints on steering angle
-        self.mpc.bounds['lower','_u','delta'] = -0.7
-        self.mpc.bounds['upper','_u','delta'] = 0.7
+        self.mpc.bounds['lower','_u','delta'] = - self.params['max_steering_angle']
+        self.mpc.bounds['upper','_u','delta'] = self.params['max_steering_angle']
 
-        self.mpc.bounds['lower','_u','a'] = -1
-        self.mpc.bounds['upper','_u','a'] = 5
+        self.mpc.bounds['lower','_u','a'] = - self.params['max_decel']
+        self.mpc.bounds['upper','_u','a'] = self.params['max_accel']
 
+        self.mpc.bounds['lower','_x','v'] = 0 #not going backwards
+        self.mpc.bounds['upper','_x','v'] = self.params['max_speed']
 
         self.mpc.setup()
         self.simulator = do_mpc.simulator.Simulator(self.model)
@@ -213,6 +240,7 @@ class MPC:
         
 
 def main(args):
+    
     
     rospy.init_node("mpc_node", anonymous=True)
     rospy.loginfo("starting up mpc node")
