1a3d2f629cad349536b2b47085443f2535d8f4c7
FionaLapp <fiona.lapp@web.de>
2022-06-10 17:22:21 +0100

    base structure for ftg

--- src/racing/mpc_ftg.py
+++ src/racing/mpc_ftg.py
@@ -75,7 +75,43 @@ class FTGController(mpc_base_code.BaseController):
             self.make_mpc_step(self.state)
         except AttributeError:
             print("Initialisation not finished")
+    
+    
+    def preprocess_lidar(self, ranges):
+        """ Preprocess the LiDAR scan array. Expert implementation includes:
+            1.Setting each value to the mean over some window
+            2.Rejecting high values (eg. > 3m)
+        """
+        proc_ranges = ranges
+        return proc_ranges
 
+    def find_max_gap(self, free_space_ranges):
+        """ Return the start index & end index of the max gap in free_space_ranges
+        """
+        return None
+    
+    def find_best_point(self, start_i, end_i, ranges):
+        """Start_i & end_i are start and end indicies of max-gap range, respectively
+        Return index of best point in ranges
+	Naive: Choose the furthest point within ranges and go there
+        """
+        return None
+
+    def lidar_callback(self, data):
+        """ Process each LiDAR scan as per the Follow Gap algorithm & publish an AckermannDriveStamped Message
+        """
+        ranges = data.ranges
+        proc_ranges = self.preprocess_lidar(ranges)
+
+        #Find closest point to LiDAR
+
+        #Eliminate all points inside 'bubble' (set them to zero) 
+
+        #Find max length gap 
+
+        #Find the best point in the gap 
+
+        #Publish Drive message
     
         
   
