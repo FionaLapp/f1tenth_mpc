c8e463634f02c26abb8c892d13927daeea627b10
FionaLapp <fiona.lapp@web.de>
2022-06-13 12:05:03 +0100

    Working on visualisation

--- src/racing/mpc_ftg.py
+++ src/racing/mpc_ftg.py
@@ -32,6 +32,7 @@ class FTGController(mpc_base_code.BaseController):
         self.t_x=0
         self.t_y=0
         self.state=[0,0,0]
+        self.laser_data=None
         super().__init__()
         self.setup_laser_scan()
         
@@ -73,7 +74,8 @@ class FTGController(mpc_base_code.BaseController):
         kernel_size = 4
         kernel = np.ones(kernel_size) / kernel_size
         ranges_convolved = np.convolve(ranges, kernel, mode='same') #averaging over every 4 elements
-        proc_ranges = np.where(ranges_convolved<=3, ranges_convolved, 3) #set everything larger than 3 to 3
+        max_range=7
+        proc_ranges = np.where(ranges_convolved<=max_range, ranges_convolved, max_range) #set everything larger than 3 to 3
         return proc_ranges
 
     def find_max_gap(self, ranges):
@@ -94,49 +96,65 @@ class FTGController(mpc_base_code.BaseController):
 	Naive: Choose the furthest point within ranges and go there
         """
         gap_array=range
-        #return int(end_i-start_i/2)
-        furthest_point_index=np.argmax(gap_array)
-        return  furthest_point_index
+        return int((end_i-start_i)/2+start_i)
+        # furthest_point_index=np.argmax(gap_array)+start_i
+        # return  furthest_point_index
 
-    def lidar_to_xy(self, ranges, index, angle_increment, angle_min):
-        laser_angle = (index * angle_increment) + angle_min
-        heading_angle=self.state[2]
-        rotated_angle = laser_angle + heading_angle
-        p_x = ranges[index] * np.cos(rotated_angle) + self.state[0]
-        p_y = ranges[index]  * np.sin(rotated_angle) + self.state[1]
-        return p_x, p_y
+    def lidar_to_xy(self, ranges, index):
+        if self.laser_data is None:
+            print("no laser data provided")
+        else:
+            laser_angle = (index * self.laser_data.angle_increment) + self.laser_data.angle_min
+            heading_angle=self.state[2]
+            rotated_angle = laser_angle + heading_angle
+            p_x = self.proc_ranges[index] * np.cos(rotated_angle) + self.state[0]
+            p_y = self.proc_ranges[index]  * np.sin(rotated_angle) + self.state[1]
+            return p_x, p_y
         
 
 
     def lidar_callback(self, data:LaserScan):
         """ Process each LiDAR scan as per the Follow Gap algorithm & publish an AckermannDriveStamped Message
         """
+        self.laser_data=data
         ranges = data.ranges
 
-        proc_ranges = self.preprocess_lidar(ranges)
+        self.proc_ranges = self.preprocess_lidar(ranges)
 
         #Find closest point to LiDAR
-        closest_point_index=np.argmin(proc_ranges)
+        closest_point_index=np.argmin(self.proc_ranges)
 
         #Eliminate all points inside 'bubble' (set them to zero) 
         bubble_radius=3
-        proc_ranges[closest_point_index-bubble_radius:closest_point_index+bubble_radius]=0
+        self.proc_ranges[closest_point_index-bubble_radius:closest_point_index+bubble_radius]=0
  
         #Find max length gap 
-        start_index, end_index=self.find_max_gap(proc_ranges)
+        start_index, end_index=self.find_max_gap(self.proc_ranges)
 
         #Find the best point in the gap 
-        best_point_index=self.find_best_point_index(start_index, end_index, proc_ranges)
+        best_point_index=self.find_best_point_index(start_index, end_index, self.proc_ranges)
         
         #calculate x and y value, given the index of the lidar point
+
         
-        self.t_x, self.t_y=self.lidar_to_xy(proc_ranges, best_point_index, data.angle_increment, data.angle_min)
-        start_x, start_y=self.lidar_to_xy(proc_ranges, start_index, data.angle_increment, data.angle_min)
-        end_x, end_y=self.lidar_to_xy(proc_ranges, end_index, data.angle_increment, data.angle_min)
-        vis_point=visualiser.GapMarker([start_x, end_x], [start_y, end_y], 1)
-        print("x:{}, y:{}".format(self.t_x, self.t_y))
+        self.t_x, self.t_y=self.lidar_to_xy(self.proc_ranges, best_point_index)
+        gap_x_array=[]
+        gap_y_array=[]
+        for i in range(start_index, end_index):
+            temp_x, temp_y=self.lidar_to_xy(self.proc_ranges, i)
+            gap_x_array.append(temp_x)
+            gap_y_array.append(temp_y)
+        gap_line=visualiser.GapMarker(gap_x_array, gap_y_array, 1)
+        # start_x, start_y=self.lidar_to_xy(self.proc_ranges, start_index)
+        # end_x, end_y=self.lidar_to_xy(self.proc_ranges, end_index)
+        # gap_line=visualiser.GapMarker([start_x, end_x], [start_y, end_y], 1)
+        #print("x:{}, y:{}".format(self.t_x, self.t_y))
         #vis_point=visualiser.TargetMarker(self.path_data_x[self.index:self.index+self.n_horizon], self.path_data_y[self.index:self.index+self.n_horizon], 1)
-        vis_point.draw_point()
+        gap_line.draw_point()
+        gap_point=visualiser.TargetMarker(self.t_x, self.t_y, 1)
+        #print("x:{}, y:{}".format(self.t_x, self.t_y))
+        #vis_point=visualiser.TargetMarker(self.path_data_x[self.index:self.index+self.n_horizon], self.path_data_y[self.index:self.index+self.n_horizon], 1)
+        gap_point.draw_point()
 
     def prepare_goal_template(self, t_now):
         
@@ -146,6 +164,7 @@ class FTGController(mpc_base_code.BaseController):
             
             template["_tvp", k, "target_x"]=self.t_x
             template["_tvp", k, "target_y"] =self.t_y
+        
         return template     
   
 
