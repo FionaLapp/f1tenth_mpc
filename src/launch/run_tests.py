37d072998b06b0c793f7fdfc384a62728a0de7f9
FionaLapp <fiona.lapp@web.de>
2022-08-01 16:33:12 +0200

    R-delta-tests done

--- src/launch/run_tests.py
+++ src/launch/run_tests.py
@@ -8,21 +8,21 @@ import sys
 # open the file in the write mode
 cwd=sys.path[0]
 print(cwd)
-csv_filepath=cwd+"/../logs/r-delta-tests_ftg"
-header=["node_type","r_v", "r_delta", "n_horizon", "velocity", "world_name", "include obstacles", "lap1", "lap2","lap3", "lap4", "lap5", "collisions", "dnf", "mean_laptime", "std_dev"]
+csv_filepath=cwd+"/../logs/r-v-tests_ftg"
+header=["node_type","r_v", "r_delta", "n_horizon", "velocity", "world_name", "include obstacles", "lap1", "lap2","lap3", "collisions", "dnf", "mean_laptime", "std_dev"]
 with open(csv_filepath, 'w') as f:
     # create the csv writer
     writer = csv.writer(f)
 
     # write a row to the csv file
     writer.writerow(header)
-r_delta_list=linspace(0.5, 2.5, 5)
-print(r_delta_list)
-for r in r_delta_list: #timeout: 5 min (60*5/5)
+param_list=[0.1,0.2,0.3,0.4,0.5,1.0,1.5,2.0]
+print(param_list)
+for r in param_list: #timeout: 5 min (60*5/5)
     if rosgraph.is_master_online(): # Checks the master uri and results boolean (True or False)
         print('ROS MASTER is Online')
     else:
-        os.system("roslaunch f1tenth_mpc full_simulation.launch node_type:=ftg velocity:='7' r_delta:={} log_file_name:={}".format(r, csv_filepath))
+        os.system("roslaunch f1tenth_mpc full_simulation.launch node_type:=ftg velocity:='7' r_v:={} log_file_name:={}".format(r, csv_filepath))
 
     
 os.system("killall -9 rosmaster")
