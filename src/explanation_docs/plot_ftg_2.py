a3d9cf5a87268d52a66ca9439471063060ad83f3
FionaLapp <fiona.lapp@web.de>
2022-08-01 12:07:06 +0200

    Some plotting for ftg

---
+++ src/explanation_docs/plot_ftg_2.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import csv 
+import sys
+import pandas as pd
+
+cwd=sys.path[0]
+print(cwd)
+ranges=[]
+
+csv_filepath=cwd+"/ftg_ranges_example.csv"
+#ranges=pd.read_csv(csv_filepath)
+with open(csv_filepath, newline='') as csvfile:
+    data=csvfile.read()
+data_list=data.split(",")
+r_array=np.zeros(len(data_list))
+for i in range(len(data_list)):
+    r_array[i]=float(data_list[i].strip())
+
+x_array=np.linspace(-np.pi, np.pi, num=len(r_array), endpoint=True, retstep=False, dtype=None, axis=0)
+r_array=np.where(r_array<5, r_array, 0*r_array)
+fig=plt.figure(figsize=(10,10))
+plt.xlabel("lidar angle (rad)")
+plt.ylabel("lidar range (m)")
+plt.title("Plot of lidar range data vs angle")
+plt.plot(x_array, r_array)
+plt.show()
+plt.savefig(cwd+"/ftg_plot.png")
+print("done")
