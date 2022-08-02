590cfc12213e420f2e0c0f5086b94372918a0776
FionaLapp <fiona.lapp@web.de>
2022-08-02 19:54:03 +0200

    v weight centerline

--- src/launch/run_tests.py
+++ src/launch/run_tests.py
@@ -8,7 +8,7 @@ import sys
 # open the file in the write mode
 cwd=sys.path[0]
 print(cwd)
-csv_filepath=cwd+"/../logs/horizon_centerline"
+csv_filepath=cwd+"/../logs/v_weight_centerline"
 header=["node_type","velocity_weight","r_v", "r_delta", "n_horizon", "velocity", "world_name", "include obstacles", "lap1", "lap2","lap3", "collisions", "dnf", "mean_laptime", "std_dev"]
 with open(csv_filepath, 'w') as f:
     # create the csv writer
@@ -16,14 +16,14 @@ with open(csv_filepath, 'w') as f:
 
     # write a row to the csv file
     writer.writerow(header)
-param_list=[7,8]
+param_list=[1.2,1.3,1.4,1.5,1.6,1.7,1.8]
 print(param_list)
 for r in param_list: #timeout: 5 min (60*5/5)
     print(r)
     if rosgraph.is_master_online(): # Checks the master uri and results boolean (True or False)
         print('ROS MASTER is Online')
     else:
-        os.system("roslaunch f1tenth_mpc full_simulation.launch node_type:=centerline velocity:='7' n_horizon:={} log_file_name:={}".format(r, csv_filepath))
+        os.system("roslaunch f1tenth_mpc full_simulation.launch node_type:=centerline velocity:='7' velocity_weight:={} log_file_name:={}".format(r, csv_filepath))
 
     
 os.system("killall -9 rosmaster")
